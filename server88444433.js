73f594517dfa30f7d2f65a5c9e507978457c5717
iankilgore <ianmacduffkilgore@gmail.com>
2015-11-03 14:17:09 -0700

    127

--- server88444433.js
+++ server88444433.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req,res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Redirecting you to casper.estesschools.org \n');
-}) .listen(8443, "172.31.20.140");
+}) .listen(8443, "127.0.0.1");
 console.log('Server running at http://172.31.20.140:8443/');
